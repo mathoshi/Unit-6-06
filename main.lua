e2edb4cf0bc197e3949ba4d4ea0e28286104f842
mathoshi <47573361+mathoshi@users.noreply.github.com>
2019-04-12 13:17:09 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,6 +6,15 @@
 -- This file will do loops
 -----------------------------------------------------------------------------------------
 
-for lopp = 1, 4 do
-	print(lopp)
+local background = display.setDefault( "background", 1, 1, 1)
+
+local girl = display.newImageRect( "assets/girl.png", 200, 200)
+girl.x = -10
+girl.y = 200
+girl.id = "Girl"
+
+for loopCounter = 1, 10 do
+	print(loopCounter)
+	girl.x = girl.x + 10
+
 end
