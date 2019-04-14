04598feaca27708c7b4e8e4a8f834282394ece91
mathoshi <47573361+mathoshi@users.noreply.github.com>
2019-04-13 21:33:42 -0400

    Update 1
    Made it count to 21, not 10.

--- main.lua
+++ main.lua
@@ -6,14 +6,14 @@
 -- This file will do loops
 -----------------------------------------------------------------------------------------
 
-local background = display.setDefault( "background", 1, 1, 1)
+local background = display.setDefault( "background", 237/255, 237/255, 237/255)
 
 local girl = display.newImageRect( "assets/girl.png", 200, 200)
 girl.x = -10
 girl.y = 200
-girl.id = "Girl"
+girl.id = "girl"
 
-for loopCounter = 1, 10 do
+for loopCounter = 1, 21 do
 	print(loopCounter)
 	girl.x = girl.x + 10
 
